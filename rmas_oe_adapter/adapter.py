0ce993bc7a05fa016ea4c062eaf50ff3ebc9e279
Jason Marshall <ctrlspc@gmail.com>
2012-10-18 12:26:06 +0100

    parsed the initial project information
    need to work out how to parse the prinicple_investigator

--- rmas_oe_adapter/adapter.py
+++ rmas_oe_adapter/adapter.py
@@ -4,18 +4,36 @@ Created on Oct 17, 2012
 @author: jasonmarshall
 '''
 import logging
+from lxml import etree
 
 def handle_event(event):
     '''
         @param event the raw string of the rmas event
     '''
-    
     logging.info('handling event: %s' % event)
     
-    #check to see if the event is a proposal created event
+    parser = etree.XMLParser(remove_comments=True)
+    event_root = etree.fromstring(str(event), parser=parser)
     
+    #check to see if the event is a proposal created event
+    event_type = event_root.xpath('/rmas/message-type').pop().text
+    
+    if event_type.lower() == 'proposal-created':
+        logging.info('Got proposal created message!')
         #if so retrieve the key information from the cerif payload:
+        projid = event_root.xpath('/rmas/p:CERIF/p:cfProj/p:cfProjId/.', 
+                                  namespaces={'p':'urn:xmlns:org:eurocris:cerif-1.4-0'}).pop().text
+        projtitle = event_root.xpath('/rmas/p:CERIF/p:cfProj/p:cfTitle/.',
+                                      namespaces={'p':'urn:xmlns:org:eurocris:cerif-1.4-0'}).pop().text
         
+        principle_investigator_person_id = None
+        principle_investigator_person_node = None
+        
+        logging.info('''Got details: 
+        projectId=%s
+        projectTitle=%s
+        principleInvestigator=%s
+        '''% (projid,projtitle,'unknown'))
         #call the OpenEthics API with the information retrieved from the payload
         
         #persist a link between the proposal uuid and the application id returned by the api call
