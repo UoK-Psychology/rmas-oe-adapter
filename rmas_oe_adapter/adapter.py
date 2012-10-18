98403cb18ad67159a85e9555c90f2c1c7b00382d
Jason Marshall <ctrlspc@gmail.com>
2012-10-18 11:25:49 +0100

    added psudocode for initial adapter implementation

--- rmas_oe_adapter/adapter.py
+++ rmas_oe_adapter/adapter.py
@@ -11,3 +11,11 @@ def handle_event(event):
     '''
     
     logging.info('handling event: %s' % event)
+    
+    #check to see if the event is a proposal created event
+    
+        #if so retrieve the key information from the cerif payload:
+        
+        #call the OpenEthics API with the information retrieved from the payload
+        
+        #persist a link between the proposal uuid and the application id returned by the api call
