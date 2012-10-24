5226e5e36c7b56f87d7abf2d29a10ffec290e7d7
Jason Marshall <ctrlspc@gmail.com>
2012-10-24 12:44:54 +0100

    fixed the xpaths which got broken in the last refactor
    and hardcoded the principle email address (to allow for an end to end
    test)

--- rmas_oe_adapter/parser.py
+++ rmas_oe_adapter/parser.py
@@ -35,21 +35,27 @@ def parse_proposal_payload(payload):
         
         {'project_id':'',
         'project_title':'',
-        'principle_investigator_id':''}
+        'principle_investigator_id':'',
+        'principle_investigator_principle_email':''}
         
     '''
     
-    projid = payload.xpath('/p:CERIF/p:cfProj/p:cfProjId/.', 
+    projid = payload.xpath('p:cfProj/p:cfProjId', 
                               namespaces={'p':'urn:xmlns:org:eurocris:cerif-1.4-0'}).pop().text
-    projtitle = payload.xpath('/p:CERIF/p:cfProj/p:cfTitle/.',
+    projtitle = payload.xpath('p:cfProj/p:cfTitle',
                                   namespaces={'p':'urn:xmlns:org:eurocris:cerif-1.4-0'}).pop().text
     
-    principle_investigator_person_id = payload.xpath("/p:CERIF/p:cfProj/p:cfProj_Pers[p:cfClassId='b0e11470-1cfd-11e1-8bc2-0800200c9a66']/p:cfPersId", 
+    principle_investigator_person_id = payload.xpath("p:cfProj/p:cfProj_Pers[p:cfClassId='b0e11470-1cfd-11e1-8bc2-0800200c9a66']/p:cfPersId", 
                                                   namespaces={'p':'urn:xmlns:org:eurocris:cerif-1.4-0'}).pop().text
     
-    proposal_details = {'proposal_id':projid,
-    'project_title':projtitle,
-    'principle_investigator_id':principle_investigator_person_id}
+    principle_investigator_principle_email = 'me@home.com'
+    
+    proposal_details = {
+                        'proposal_id':projid,
+                        'project_title':projtitle,
+                        'principle_investigator_id':principle_investigator_person_id,
+                        'principle_investigator_principle_email':principle_investigator_principle_email
+                        }
     
     logging.info('Got details: %s' % proposal_details)
     
