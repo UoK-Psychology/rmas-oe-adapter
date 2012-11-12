d19a120c5a175392bae0e035c470dd45c1f619be
Jason Marshall <ctrlspc@gmail.com>
2012-11-12 15:07:26 +0000

    implemented a function to lookup the
    proposal_ethics_application_link (this will be used upon receiveing a
    lifecylce event).

--- rmas_oe_adapter/mapping.py
+++ rmas_oe_adapter/mapping.py
@@ -30,6 +30,31 @@ def persist_proposal_ethics_application_link(proposal_id, ethics_application_id)
     except PyMongoError as e:
         logging.error('An error occured trying to persist the proposal-ethics-application link: %s' % e)
 
+
+def get_proposal_ethics_application_link(**kwargs):
+    '''
+        This function returns the proposal_ethics_application link, filtered by the kwargs.
+        If no link is found this will return None.
+        
+        kwargs:
+            ethics_application_id : search for the link by the ethics id
+            proposal_id : search by the proposal id.
+    '''
+    
+    try:
+        connection = Connection()
+        database = connection.oe_rmas_adapter
+        link_collection = database.application_links
+        
+        link = link_collection.find_one(kwargs)
+        connection.close()
+        
+        return link
+        
+    except PyMongoError as e:
+        logging.error('An error occured trying to lookup the link: %s' % e)
+
+
 def persist_rmas_user_ethics_user_link(rmas_user_id, ethics_user_id):
     '''
         This function persists the link between the unique identifier that is used to identify a user
