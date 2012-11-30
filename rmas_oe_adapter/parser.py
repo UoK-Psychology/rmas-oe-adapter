e1886dbff05be1737c9003e90452792d738a3807
Jason Marshall <ctrlspc@gmail.com>
2012-11-30 09:44:59 +0000

    moved the parsing of the proposal-created message to the handler module

--- rmas_oe_adapter/parser.py
+++ rmas_oe_adapter/parser.py
@@ -8,38 +8,6 @@ import os
 from rmas_adapter.conf import settings
 
 
-def parse_proposal_payload(payload):
-    '''
-        Parses the cerif payload contatined within the proposal events.
-        
-        It will pull out the releavant details and return a dictionary:
-        
-        {'project_id':'',
-        'project_title':'',
-        'principle_investigator_id':'',
-        'principle_investigator_principle_email':''}
-        
-    '''
-    
-    projid = payload.xpath('p:cfProj/p:cfProjId', 
-                              namespaces={'p':'urn:xmlns:org:eurocris:cerif-1.4-0'}).pop().text
-    projtitle = payload.xpath('p:cfProj/p:cfTitle',
-                                  namespaces={'p':'urn:xmlns:org:eurocris:cerif-1.4-0'}).pop().text
-    
-    principle_investigator_person_id = payload.xpath("p:cfProj/p:cfProj_Pers[p:cfClassId='b0e11470-1cfd-11e1-8bc2-0800200c9a66']/p:cfPersId", 
-                                                  namespaces={'p':'urn:xmlns:org:eurocris:cerif-1.4-0'}).pop().text
-    
-    
-    
-    proposal_details = {
-                        'proposal_id':projid,
-                        'project_title':projtitle,
-                        'principle_investigator_id':principle_investigator_person_id,
-                        }
-    
-    logging.info('Got details: %s' % proposal_details)
-    
-    return proposal_details
 
 
 
