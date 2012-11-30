df3ca3fb95bd694375288a741fd5acfbfebad929
Jason Marshall <ctrlspc@gmail.com>
2012-11-30 09:53:51 +0000

    reduced logging level

--- rmas_oe_adapter/runner.py
+++ rmas_oe_adapter/runner.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     #this has to come first before you import any other modules otherwsise the settings won't be intitialized
     os.environ.setdefault("RMAS_ADAPTER_SETTINGS", "settings")
     
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
     
     from rmas_adapter.core.poller import start_polling
     from rmas_oe_adapter import lifecycle_consumer
