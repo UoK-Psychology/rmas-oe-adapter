c808167f7305ec7929f580e320ed2dd8d7c6a252
Jason Marshall <ctrlspc@gmail.com>
2012-12-20 12:14:21 +0000

    pushed up the RMAS adapter version to latest

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     install_requires=[
         "pymongo == 2.3",
         "requests == 0.14.1",
-        "RMASAdapter == 0.1.2 ",
+        "RMASAdapter == 0.1.3 ",
         "pika==0.9.8",
     ],
 )
